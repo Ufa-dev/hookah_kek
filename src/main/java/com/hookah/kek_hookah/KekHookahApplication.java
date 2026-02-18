5ed59186c7ec68d2bfa10cca261964da1868da58
sullssul <sulem210@gmail.com>
2026-02-18 19:47:37 +0300

    init

---
+++ src/main/java/com/hookah/kek_hookah/KekHookahApplication.java
@@ -0,0 +1,13 @@
+package com.hookah.kek_hookah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KekHookahApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KekHookahApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hookah/kek_hookah/KekHookahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hookah.kek_hookah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KekHookahApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
