5fcd30f3c0c09aa68b9df28566ed24fc8c52de3f
Сулейманов Сулейман Айдарович <ssuleimanov@sberbank-tele.com>
2026-03-20 21:55:58 +0300

    fix: FlavorTagEvent extends FlavorTagEvent instead of BrandEvent

--- src/main/kotlin/com/hookah/kek_hookah/feature/tobacco/flavor/model/FlavorTagEvent.kt
+++ src/main/kotlin/com/hookah/kek_hookah/feature/tobacco/flavor/model/FlavorTagEvent.kt
@@ -1,6 +1,5 @@
 package com.hookah.kek_hookah.feature.tobacco.flavor.model
 
-import com.hookah.kek_hookah.feature.tobacco.brand.model.BrandEvent
 import com.hookah.kek_hookah.infrastructure.event.Event
 import java.time.OffsetDateTime
 
@@ -9,9 +8,9 @@ interface FlavorTagEvent : Event
 data class FlavorTagCreatedEvent(
     val flavorTag: FlavorTag,
     override val publishedAt: OffsetDateTime
-) : BrandEvent
+) : FlavorTagEvent
 
 data class FlavorTagDeleteEvent(
     val flavorTag: FlavorTag,
     override val publishedAt: OffsetDateTime
-) : BrandEvent
+) : FlavorTagEvent
